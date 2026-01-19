a471c2f8d600d0b5449f823ae1b1040fcf92b6bc
Hex <hex@codeigniter.org.cn>
2026-01-20 02:20:18 +0800

    Update smzdm_task.js

--- smzdm_task.js
+++ smzdm_task.js
@@ -63,7 +63,7 @@ class SmzdmNormalTaskBot extends SmzdmTaskBot {
       let tasks = [];
 
       if (data.data.rows[0]) {
-        data.data.rows[0].cell_data.activity_task.accumulate_list.task_list_v2.forEach(item => {
+        data.data.rows[0].cell_data.activity_task.default_list_v2.forEach(item => {
           tasks = tasks.concat(item.task_list);
         });
 
